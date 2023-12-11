a1930d66289209000886451e3c8a472fb0e7fa95
Vladyslava Nikitchenko <dn210690nvs@gmail.com>
2023-12-11 20:05:42 +0100

    feat: add code for slide in home page

--- coffee-house/pages/home/script.js
+++ coffee-house/pages/home/script.js
@@ -12,3 +12,39 @@ burger.forEach((item) => {
   });
 });
 
+
+/* Slide img */
+const img = Array.from(document.querySelectorAll('.favourite-coffee-image-container'))
+const btnNext = document.querySelector('.arrow-left');
+const btnPrev = document.querySelector('.arrow-right');
+let slideIndx = 1;
+
+showSlides(slideIndx)
+
+function showSlides(n) {
+    if(n > img.length){
+        slideIndx = 1;
+    }
+
+    if(n < 1){
+        slideIndx = img.length;
+    }
+
+    img.forEach(item => {
+        item.style.display = "none"
+    });
+
+    img[slideIndx -1].style.display = "block"
+}
+
+function addSlides(n) {
+    showSlides(slideIndx += n)
+}
+
+btnNext.addEventListener('click', ()=>{
+    addSlides(1)
+})
+btnPrev.addEventListener('click', ()=>{
+    addSlides(-1)
+})
+
