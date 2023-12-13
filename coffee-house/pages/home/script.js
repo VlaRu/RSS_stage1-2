666c376edf1b5cce901f86c2b34d6d0c1ff75d41
Vladyslava Nikitchenko <dn210690nvs@gmail.com>
2023-12-13 12:38:28 +0100

    feat: realize smoothly hides to the right when clicking on any link

--- coffee-house/pages/home/script.js
+++ coffee-house/pages/home/script.js
@@ -2,7 +2,22 @@
 
 const burgers = Array.from(document.querySelectorAll('.burger-icon-container'));
 const burgerMenus = Array.from(document.querySelectorAll('.navigation-mobile-container'));
+const linkElements = Array.from(document.querySelectorAll('.link-element'));
+console.log(linkElements)
 
+linkElements.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    burgers.forEach((otherBurger, otherIndex) => {
+      if (otherIndex !== index) {
+        otherBurger.classList.remove('active');
+        toggleBurgerMenu(otherIndex);
+      }
+    });
+
+    item.classList.toggle('active');
+    toggleBurgerMenu(index);
+  });
+});
 function toggleBurgerMenu(index) {
   burgerMenus[index].style.right = burgerMenus[index].style.right === '0%' ? '-100%' : '0%';
 }
