4e1e92781423c7a17e34a99e5a5c44c45b65abe0
VlaRu <dn210690nvs@gmail.com>
2024-01-28 20:23:44 +0100

    feat: create file with data game

---
+++ nonograms/src/nonogramData.js
@@ -0,0 +1,10 @@
+const nonogramData = {
+  gridSize: [5, 5],
+  clues: {
+    rows: [[4], [3], [1], [1], [3, 1,2, 1,2]],
+    columns: [[2], [2, 1], [2, 1], [3, 1], [1, 1]],
+  },
+  answer: [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1],
+};
+
+export default nonogramData;
