5965f6b43c638abf9652d8f45c80d3cfbe4f262b
VlaRu <dn210690nvs@gmail.com>
2024-02-04 22:36:31 +0100

    feat: implement timer for game

--- nonograms/src/script.js
+++ nonograms/src/script.js
@@ -5,7 +5,9 @@ import { handleRowFieldClick, toggleCrossCell } from "./fillCells";
 import { showDropList } from "./showDropList";
 import { clearField, clearContainer } from "./restoreGame";
 import { fillSolution } from "./fillSolutionField";
-import { switchTheme } from './switchTheme';
+import { switchTheme } from "./switchTheme";
+import { createModal } from "./modalWindow";
+import { createTimer } from "./timer";
 import {
   renderColClues,
   renderRowClues,
@@ -15,19 +17,20 @@ import {
 let index = 0;
 let titleGame = nonogramData[index].title;
 let answerGame = nonogramData[index].answer;
+let timerID = null;
 
 function createContainers() {
-  const mainContaner = document.createElement('div');
-  mainContaner.className = 'main-container';
+  const mainContaner = document.createElement("div");
+  mainContaner.className = "main-container";
 
-  const labelForInput = document.createElement('label');
-  labelForInput.className = 'switch-wrapper';
-  const switchInput = document.createElement('input');
-  switchInput.className = 'switch-input';
-  switchInput.setAttribute('type', 'checkbox');
-  switchInput.setAttribute('id', 'darkModeToggle');
-  const switchSlider = document.createElement('span');
-  switchSlider.classList.add('switch-slider', 'switch-round');
+  const labelForInput = document.createElement("label");
+  labelForInput.className = "switch-wrapper";
+  const switchInput = document.createElement("input");
+  switchInput.className = "switch-input";
+  switchInput.setAttribute("type", "checkbox");
+  switchInput.setAttribute("id", "darkModeToggle");
+  const switchSlider = document.createElement("span");
+  switchSlider.classList.add("switch-slider", "switch-round");
 
   const gameToolsContainer = document.createElement("div");
   gameToolsContainer.className = "container_game-tools";
@@ -72,29 +75,39 @@ function createContainers() {
   renderRowClues(cluesRowsContainer, index);
   renderColClues(cluesColumnContainer, index);
   renderGameField(gameFieldContainer, index);
+  function startGameTimer() {
+    const counterEl = document.querySelector(".counter");
+    let seconds = 0;
+    timerID = setInterval(() => {
+      seconds++;
+      const minutes = Math.floor(seconds / 60);
+      const remainingSeconds = seconds % 60;
+      const display = `${minutes}:${
+        remainingSeconds < 10 ? "0" : ""
+      }${remainingSeconds}`;
+      counterEl.innerText = display;
+    }, 1000);
+  }
+
+  function stopGameTimer() {
+    clearInterval(timerID);
+    const display = document.querySelector(".counter").innerText;
+    document.querySelector(
+      ".win-game-time"
+    ).innerText = `Your time is ${display}`;
+  }
+
+  function clearGameTimer() {
+    clearInterval(timerID);
+    document.querySelector('.counter').innerText = '00:00';
+  }
   document.addEventListener("click", (event) => {
     if (event.target.classList.contains("drop-element")) {
       index = parseInt(event.target.dataset.index, 10);
-      clearContainer(cluesRowsContainer);
-      clearContainer(cluesColumnContainer);
-      clearContainer(gameFieldContainer);
-      renderRowClues(cluesRowsContainer, index);
-      renderColClues(cluesColumnContainer, index);
-      renderGameField(gameFieldContainer, index);
-      titleGame = nonogramData[index].title;
-      nameGame.textContent = `${titleGame}`;
-      answerGame = nonogramData[index].answer;
+      renderGame(index);
     } else if (event.target.classList.contains("random-game_button")) {
       index = getRandomIndex();
-      clearContainer(cluesRowsContainer);
-      clearContainer(cluesColumnContainer);
-      clearContainer(gameFieldContainer);
-      renderRowClues(cluesRowsContainer, index);
-      renderColClues(cluesColumnContainer, index);
-      renderGameField(gameFieldContainer, index);
-      titleGame = nonogramData[index].title;
-      nameGame.textContent = `${titleGame}`;
-      answerGame = nonogramData[index].answer;
+      renderGame(index);
     } else if (event.target.classList.contains("restore-game_button")) {
       clearField();
     } else if (event.target.classList.contains("solution-game_button")) {
@@ -103,13 +116,53 @@ function createContainers() {
     }
   });
 
-  renderDropElements(dropDownContent);
+  document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("row-field")) {
+      if (!timerID) {
+        startGameTimer();
+      }
+    }
+  });
 
+  function renderGame(i) {
+    clearContainer(cluesRowsContainer);
+    clearContainer(cluesColumnContainer);
+    clearContainer(gameFieldContainer);
+    renderRowClues(cluesRowsContainer, i);
+    renderColClues(cluesColumnContainer, i);
+    renderGameField(gameFieldContainer, i);
+    titleGame = nonogramData[index].title;
+    nameGame.textContent = `${titleGame}`;
+    answerGame = nonogramData[index].answer;
+  }
+
+  function handleGameControlsClick(event) {
+    if (
+      event.target.classList.contains("button_next-game") ||
+      event.target.classList.contains("close-modal") ||
+      event.target.classList.contains("modal-container")
+    ) {
+      document.querySelector(".modal-container").style.display = 'none';
+      event.stopPropagation();
+      index += 1;
+      renderGame(index);
+      stopGameTimer();
+      console.log("Event handler is called");
+      clearGameTimer()
+    }
+  }
+
+  document.addEventListener("click", (event) => {
+    handleGameControlsClick(event);
+  });
+
+  createModal(mainContaner);
   gameFieldFixing.appendChild(cluesRowsContainer);
   gameFieldFixing.appendChild(gameFieldContainer);
   gameContainer.appendChild(cluesColumnContainer);
   gameContainer.appendChild(gameFieldFixing);
   gameToolsContainer.appendChild(buttonContainer);
+  renderDropElements(dropDownContent);
   buttonContainer.appendChild(dropDawnContainer);
   dropDawnContainer.appendChild(chooseButton);
   dropDawnContainer.appendChild(dropDownContent);
@@ -118,6 +171,7 @@ function createContainers() {
   buttonContainer.appendChild(solutionButton);
   gameToolsContainer.appendChild(heading);
   gameToolsContainer.appendChild(nameGame);
+  createTimer(gameToolsContainer);
   labelForInput.appendChild(switchInput);
   labelForInput.appendChild(switchSlider);
   mainContaner.appendChild(labelForInput);
