fd82a3d82bb3add3920d21adf7419edb7d8b2115
VlaRu <dn210690nvs@gmail.com>
2024-02-04 22:29:23 +0100

    refactor: realize close drop list after event click on main container

--- nonograms/src/showDropList.js
+++ nonograms/src/showDropList.js
@@ -2,6 +2,11 @@ function showDropList() {
   document.querySelector('.choose-game_button').addEventListener('click', () => {
     document.querySelector('.drop-down_content').classList.toggle('show-drop-list');
   })
+  document.addEventListener('click', (event) => {
+    if (!event.target.closest('.drop-down_container')) {
+      document.querySelector('.drop-down_content').classList.remove('show-drop-list');
+    }
+  });
 }
 
 export { showDropList };
