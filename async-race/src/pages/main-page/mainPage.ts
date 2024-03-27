f4b206f2af0f445c26126b4616f2e329458f2691
VlaRu <dn210690nvs@gmail.com>
2024-03-27 22:25:26 +0100

    refactor: optimization render method in MainPage class

--- async-race/src/pages/main-page/mainPage.ts
+++ async-race/src/pages/main-page/mainPage.ts
@@ -1,8 +1,9 @@
-import NavigatePage from "./navigatePage";
-import ContainerCreateCar from "./creatingCar";
-import RenderPageElements from "../../templates/pageElemTemplate";
-import PaginationPage from "./pagination";
-import RaceContainer from "./raceContainer";
+import NavigatePage from './mainPage-components/navigatePage';
+import ContainerCreateCar from './mainPage-components/creatingCar';
+import RenderPageElements from '../../templates/pageElemTemplate';
+import PaginationPage from './mainPage-components/pagination';
+import RaceContainer from './mainPage-components/raceContainer';
+
 export default class MainPage {
   container: HTMLElement;
 
@@ -11,16 +12,20 @@ export default class MainPage {
     this.container.id = id;
   }
 
-  render(){
+  render() {
     const titlePage = RenderPageElements.createHeaderTitle('Hello to the main page of the Async-race game');
-    this.container.append(titlePage);
     const navigatePage = new NavigatePage();
-    this.container.append(navigatePage.render());
-    this.container.append(ContainerCreateCar.formContainer());
     const paginationPage = new PaginationPage('div', '');
-    this.container.append(paginationPage.createPaginationContainer());
     const raceContainer = new RaceContainer('div', 'race-container');
-    this.container.append(raceContainer.render());
+
+    this.container.append(
+      titlePage,
+      navigatePage.render(),
+      ContainerCreateCar.formContainer(),
+      paginationPage.createPaginationContainer(),
+      raceContainer.render()
+    );
+
     return this.container;
   }
 }
