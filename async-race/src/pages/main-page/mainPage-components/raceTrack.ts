ac07063fa07cd672fe6236263a7a17a199c9064c
VlaRu <dn210690nvs@gmail.com>
2024-03-29 17:32:20 +0100

    feat: add in carContainer function for render image, add async request

--- async-race/src/pages/main-page/mainPage-components/raceTrack.ts
+++ async-race/src/pages/main-page/mainPage-components/raceTrack.ts
@@ -2,18 +2,25 @@ import RenderPageElements from '../../../templates/pageElemTemplate';
 import { renderCarImage } from './carImage';
 
 export default class RaceTrack extends RenderPageElements {
-  constructor(container: HTMLElement) {
+  private color: string;
+
+  private name: string;
+
+  constructor(container: HTMLElement, color: string, name: string) {
     super('div', 'race-track_container');
     this.container = container;
+    this.color = color;
+    this.name = name;
   }
 
-  render() {
-    const carContainer = document.createElement('div');
-    carContainer.className = 'car-container';
-    carContainer.innerHTML = renderCarImage('gray');
-
-    this.container.appendChild(carContainer);
-
-    return this.container;
+  async render() {
+      const carContainer = document.createElement('div');
+      const nameCar = document.createElement('p');
+      nameCar.innerText = this.name;
+      carContainer.className = 'car-container';
+      carContainer.innerHTML = renderCarImage(this.color);
+      this.container.appendChild(nameCar);
+      this.container.appendChild(carContainer);
+      return this.container;
   }
 }
