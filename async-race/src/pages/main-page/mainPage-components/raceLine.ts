28bfeb9ae32af347f24ebf41670f07c6328bd45e
VlaRu <dn210690nvs@gmail.com>
2024-03-29 17:36:04 +0100

    feat: implement rendering data cars from server and add creation car race line with retrieve datas

--- async-race/src/pages/main-page/mainPage-components/raceLine.ts
+++ async-race/src/pages/main-page/mainPage-components/raceLine.ts
@@ -2,15 +2,25 @@ import RenderPageElements from '../../../templates/pageElemTemplate';
 import MontainCar from './montainCar';
 import MontainRace from './montainRace';
 import RaceTrack from './raceTrack';
+import { getCars } from '../../../api/api';
 
+export interface Car {
+  name: string,
+  color: string,
+  id: number,
+}
 export default class RaceLine extends RenderPageElements {
-  render() {
-    const montainCar = new MontainCar(this.container, 2);
-    const montainRace = new MontainRace(this.container, 2);
-    const raceTrack = new RaceTrack(this.container);
-    montainCar.render();
-    montainRace.render();
-    raceTrack.render();
+  async render(): Promise<HTMLElement> {
+    const { items: cars } = await getCars();
+
+    cars.forEach((car: Car) => {
+      const montainCar = new MontainCar(this.container, car.id);
+      const montainRace = new MontainRace(this.container, car.id);
+      const raceTrack = new RaceTrack(this.container, car.color, car.name);
+      montainCar.render();
+      montainRace.render();
+      raceTrack.render();
+    });
     return this.container;
   }
 }
