e1124fc6de2dfa4337714a9f71055c60db93ed06
VlaRu <dn210690nvs@gmail.com>
2024-04-01 21:59:35 +0200

    feat: implement some html structures for creating table in winners file

---
+++ async-race/src/pages/winners-page/winnersTableElements.ts
@@ -0,0 +1,23 @@
+export const renderWinners = () =>
+`
+    <h1 class="title" >Winners</h1>
+    <h2 class="subtitle">Page</h2>
+    <table class= "table-winners">
+      <thead class = "head-table">
+        <th>Number</th>
+        <th>Car</th>
+        <th>Name</th>
+        <th>Wins</th>
+        <th>Best time</th>
+      </thead>
+      <tbody class= "body-table" >
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+      </tbody>
+    </table>
+`;
