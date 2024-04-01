c535e1e63c65d8a244198412fd12e169b6f166a1
VlaRu <dn210690nvs@gmail.com>
2024-04-01 22:08:44 +0200

    refactor: fix some mistakes in delete function, change updateCar function

--- async-race/src/api/api.ts
+++ async-race/src/api/api.ts
@@ -1,12 +1,12 @@
 const url: string = 'http://127.0.0.1:3000';
-const garageUrl: string = `${url}/garage`;
+export const garageUrl: string = `${url}/garage`;
 export const engineUrl: string = `${url}/engine`;
 export const winnersUrl: string = `${url}/winners`;
 
 export interface Car {
   name: string,
   color: string,
-  id: number,
+  id?: number,
 }
 
 export const getCar = async (carId: number) => (await fetch(`${garageUrl}/${carId}`)).json();
@@ -18,9 +18,10 @@ export const getCars = async (page: number = 1 , limit: number = 7) => {
     count: response.headers.get('X-Total-Count'),
   };
 }
+
 export const { items: cars , count: carsCount } = await getCars();
 
-export const createCar = async (body: HTMLBodyElement) => (await fetch(garageUrl, {
+export const createCar = async (body: Car) => (await fetch(garageUrl, {
   method: 'POST',
   body: JSON.stringify(body),
   headers: {
@@ -29,10 +30,10 @@ export const createCar = async (body: HTMLBodyElement) => (await fetch(garageUrl
 })).json();
 
 
-export const deleteCar = async (carId: number) => (await fetch(`${garageUrl}/${carId} , {method: 'DELETE}`)).json();
+export const deleteCar = async (carId: number) => (await fetch(`${garageUrl}/${carId}` , {method: 'DELETE'})).json();
 
-export const updateCar = async (id: number, body: HTMLBodyElement) => (await fetch(`${garageUrl}/${id}`, {
-  method: 'PUT',
+export const updateCar = async (id: number, body: Car) => (await fetch(`${garageUrl}/${id}`, {
+  method: 'PATCH',
   body: JSON.stringify(body),
   headers: {
     'Content-Type': 'application/json'
