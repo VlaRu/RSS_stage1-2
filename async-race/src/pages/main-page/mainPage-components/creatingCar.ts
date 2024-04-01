b585430a4261c2e57faf8cc74e7ec0095cb4f57a
VlaRu <dn210690nvs@gmail.com>
2024-04-01 22:25:36 +0200

    feat: implement methods for taking input value text and color after submitting, realize receive new created car with input datas on server

--- async-race/src/pages/main-page/mainPage-components/creatingCar.ts
+++ async-race/src/pages/main-page/mainPage-components/creatingCar.ts
@@ -1,14 +1,54 @@
-import FormTemplate from "../../../templates/inputTemplate";
+import FormTemplate from "../../../templates/formTemplate";
+import { createCar, Car, updateCar } from "../../../api/api";
+
+const newcarId: number = 1;
 
 export default class ContainerCreateCar {
+
   static createFormForCreatingCar(): HTMLFormElement {
     const form = FormTemplate.createForm('text', 'created_name-car', 'Create new car');
+    this.attachFormSubmitHandler(form);
     return form;
   }
 
   static createFormForModifyingCar(): HTMLFormElement {
     const form = FormTemplate.createForm('text', 'modified_name-car', 'Modify selected car');
+    this.attachFormSubmitChangedHandler(form, newcarId);
     return form;
+  }
+
+  static attachFormSubmitHandler(form: HTMLFormElement): void {
+    const handleSubmit = async (formData: { text: string, color: string }) => {
+      const newCar: Car = {
+        name: formData.text,
+        color: formData.color,
+      };
+
+      try {
+        const createdCar = await createCar(newCar);
+        console.log('Car created:', createdCar);
+      } catch (error) {
+        console.error('Error creating car:', error);
+      }
+    };
+    FormTemplate.handleFormSubmit(form, handleSubmit);
+  }
+
+  static attachFormSubmitChangedHandler(form: HTMLFormElement, carId: number): void {
+    const handleSubmit = async (formData: { text: string, color: string }) => {
+      const newCar: Car = {
+        name: formData.text,
+        color: formData.color,
+      };
+
+      try {
+        const createdCar = await updateCar(carId, newCar);
+        console.log(`Car created: ${carId}`, createdCar);
+      } catch (error) {
+        console.error('Error creating car:', error);
+      }
+    };
+    FormTemplate.handleFormSubmit(form, handleSubmit);
   }
 
   static formContainer(): HTMLDivElement {
