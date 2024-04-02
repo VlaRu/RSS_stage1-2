a8969d68038b99979eda1bb6e2ca06ef5d361456
VlaRu <dn210690nvs@gmail.com>
2024-04-02 21:51:49 +0200

    feat: perform refresh render data on the page after clicking remove page

--- async-race/src/pages/main-page/mainPage-components/buttonHandler.ts
+++ async-race/src/pages/main-page/mainPage-components/buttonHandler.ts
@@ -1,11 +1,16 @@
 import { deleteCar } from "../../../api/api";
+import { raceContainer } from "../mainPage";
 
 const onSelectClick = (carId: number) => { console.log(carId);};
 
 const onRemoveClick = async (carId: number) => {
   try {
     const removeCar = await deleteCar(carId);
+    raceContainer.container.innerHTML = '';
+    raceContainer.render()
     console.log('Car deleted:', removeCar);
+    console.log(carId);
+
   } catch (error) {
     console.error('Error delete car:', error);
   }
