09f83ee5d38bc02322c951786d6c76a98b6cee84
VlaRu <dn210690nvs@gmail.com>
2024-04-02 21:49:44 +0200

    refactor: change in literal template variable name for display page number

--- async-race/src/pages/main-page/mainPage-components/pagination.ts
+++ async-race/src/pages/main-page/mainPage-components/pagination.ts
@@ -2,18 +2,16 @@ import RenderPageElements from '../../../templates/pageElemTemplate';
 import { getCars } from '../../../api/api';
 import ButtonRenderer from '../../../templates/buttonRenderer';
 import { prevButton, nextButton } from './paginationButton';
+import { pageNumber } from './paginationButton';
 
 export let carsCount: number = 0;
 export default class PaginationPage extends RenderPageElements {
-
-  static currentPage: number = 1;
-
   async createPaginationContainer(): Promise<HTMLElement> {
-    const { count: fetchedCarsCount } = await getCars(PaginationPage.currentPage);
+    const { count: fetchedCarsCount } = await getCars(pageNumber.number);
     carsCount = parseInt(fetchedCarsCount || '0', 10);
     const paginationContainer = await this.createPageElement('div', 'pagination-container');
     const garageParagraph = await this.createPageElement('p', 'count-car_garage', `Garage: ${carsCount}`);
-    const pageParagraph = await this.createPageElement('p', 'count-page_garage', `Page: ${PaginationPage.currentPage}`);
+    const pageParagraph = await this.createPageElement('p', 'count-page_garage', `Page: ${pageNumber.number}`);
     paginationContainer.appendChild(garageParagraph);
     paginationContainer.appendChild(pageParagraph);
 
