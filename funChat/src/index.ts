5298fe7290123322b5aa429abd9c802cd6824718
VlaRu <dn210690nvs@gmail.com>
2024-04-17 21:40:26 +0200

    feat: implement router for spa

---
+++ funChat/src/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import './index.css';
+import App from './router/app';
+
+const app = new App();
+app.run();
+
---
+++ funChat/src/router/router.ts
@@ -0,0 +1,52 @@
+import ChatPage from "../pages/page-chat/mainPageChat";
+import InfoPage from "../pages/page-info/pageInfo";
+import LoginPage from "../pages/page-login/pageLogin";
+// import ErrorPage, {ErrorTypes} from "./error";
+
+export const enum PageIds {
+  InfoPageId = 'start-page',
+  ChatPageId = 'chat-page',
+  LoginPageId = 'login-page',
+}
+
+export default class Router {
+  private container = document.body;
+
+  private defaultPageId: string = 'current-page';
+
+  async  renderPage(idPage: string){
+    const currentPage = document.querySelector(`#${this.defaultPageId}`);
+    if (currentPage) {
+      currentPage.remove();
+    }
+
+    let page = null;
+    switch (idPage) {
+      case PageIds.ChatPageId:
+        page = new ChatPage(idPage);
+        break;
+      case PageIds.InfoPageId:
+        page = new InfoPage(idPage);
+        break;
+      case PageIds.LoginPageId:
+        page = new LoginPage(idPage);
+        break;
+      default:
+        page = new InfoPage(idPage);
+        break;
+    }
+
+    if (page) {
+      const pageHTML = await page.render();
+      pageHTML.id = this.defaultPageId;
+      this.container.append(pageHTML);
+    }
+  }
+
+  enableRouteChange() {
+    window.addEventListener('hashchange', () => {
+      const hash = window.location.hash.slice(1);
+      this.renderPage(hash);
+    });
+  }
+}
