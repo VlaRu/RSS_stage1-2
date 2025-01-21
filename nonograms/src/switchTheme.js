8ce9a3dd6128209f2e22c7755ddee0e4679480c5
VlaRu <dn210690nvs@gmail.com>
2025-01-21 20:43:25 +0100

    refactor: add img bg in style

--- nonograms/src/switchTheme.js
+++ nonograms/src/switchTheme.js
@@ -2,7 +2,7 @@ function switchTheme() {
   const darkModeToggle = document.getElementById('darkModeToggle');
   darkModeToggle.addEventListener('change', () => {
   document.body.classList.toggle('dark-mode');
-  document.querySelector('.container_game-tools').classList.toggle('dark-mode-bg');
+  document.querySelector('.main-container').classList.toggle('dark-mode-bg');
   document.querySelectorAll('.clues-columns__element-container').forEach((i) => i.classList.toggle('dark-clues'));
   document.querySelectorAll('.clues-rows__element-container').forEach((i) => i.classList.toggle('dark-clues'));
   });
