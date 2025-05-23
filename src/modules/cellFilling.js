ecda5a842d7da5409d7cf4b7812ac3ec5c26f1eb
vlaru <dn210690nvs@gmail.com>
2025-05-23 16:52:02 +0300

    refactor: update sell filling logic, optimize code

---
+++ src/modules/cellFilling.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-param-reassign */
+
+function fillSolution(answ) {
+  const fieldEl = document.querySelectorAll('.row-field');
+  if (fieldEl.length === answ.length) {
+    fieldEl.forEach((el, i) => {
+      if (answ[i] === 1) {
+        el.classList.add('fill-cell');
+      }
+    });
+  } else {
+    console.error('Field and answer array lengths do not match.');
+  }
+  console.log('fill');
+}
+
+function toggleFillCell(cell) {
+  if (cell.classList.contains('fill-cell')) {
+    cell.classList.remove('fill-cell');
+    cell.innerText = '';
+  } else {
+    cell.classList.add('fill-cell');
+  }
+}
+
+function toggleCrossCell(cell) {
+  if (cell.innerText === '') {
+    cell.innerText = 'X';
+    cell.classList.remove('fill-cell');
+  } else {
+    cell.innerText = '';
+  }
+}
+
+function toggleCellState(cell) {
+  if (!cell.classList.contains('fill-cell') && cell.innerText === '') {
+    cell.classList.add('fill-cell');
+    cell.innerText = '';
+  } else if (cell.classList.contains('fill-cell')) {
+    cell.classList.remove('fill-cell');
+    cell.innerText = 'X';
+  } else if (cell.innerText === 'X') {
+    cell.innerText = '';
+  }
+}
+
+export {
+  fillSolution, toggleFillCell, toggleCrossCell, toggleCellState,
+};
