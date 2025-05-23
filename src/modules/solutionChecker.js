ad7fcaf39d30b2ec132151167f49e4378d0310e7
vlaru <dn210690nvs@gmail.com>
2025-05-23 16:43:26 +0300

    refactor: move solution-checker functions to a separate module and optimize code

---
+++ src/modules/solutionChecker.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-cycle */
+import { answerGame } from './logicGame';
+import { toggleFillCell } from './cellFilling';
+
+function getHintUser() {
+  const rowFieldElements = document.getElementsByClassName('row-field');
+  const hintUser = Array.from(rowFieldElements).map((cell) => (cell.classList.contains('fill-cell') ? 1 : 0));
+  return hintUser;
+}
+
+function compareResults(hintUser) {
+  return hintUser.every((value, index) => value === answerGame[index]);
+}
+
+function displayGreat(isMatch) {
+  const modalContainer = document.querySelector('.modal-container');
+  if (isMatch) {
+    modalContainer.style.display = 'flex';
+  }
+}
+
+function checkUserInputAndDisplayResult() {
+  const hintUser = getHintUser();
+  const results = compareResults(hintUser);
+
+  setTimeout(() => {
+    displayGreat(results);
+  }, 500);
+}
+
+function handleRowFieldClick(event) {
+  if (event.target.classList.contains('row-field')) {
+    toggleFillCell(event.target);
+    checkUserInputAndDisplayResult();
+  }
+}
+
+export default handleRowFieldClick;
