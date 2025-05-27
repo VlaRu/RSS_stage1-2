0d45ee2a6a8f3aa1d2cecf9f91e9bfb8504a990a
vlaru <dn210690nvs@gmail.com>
2025-05-27 13:53:24 +0300

    refactor: enhance theme switching functionality by consolidating selectors and improving dark mode toggle logic

--- src/modules/ui/switchTheme.js
+++ src/modules/ui/switchTheme.js
@@ -1,10 +1,19 @@
 function switchTheme() {
   const darkModeToggle = document.getElementById('darkModeToggle');
+  const selectors = [
+    '.container_game-tools',
+    '.nav-elements-container',
+    '.clues-column_container',
+    '.clues-rows_container',
+    'h1',
+    'body',
+  ];
+
   darkModeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('.main-container').classList.toggle('dark-mode-bg');
-    document.querySelectorAll('.clues-columns__element-container').forEach((i) => i.classList.toggle('dark-clues'));
-    document.querySelectorAll('.clues-rows__element-container').forEach((i) => i.classList.toggle('dark-clues'));
+    document.querySelectorAll('.burger-element').forEach((el) => el.classList.toggle('dark-mode'));
+    selectors.forEach((selector) => {
+      document.querySelectorAll(selector).forEach((element) => element.classList.toggle('dark-mode'));
+    });
   });
 }
 
