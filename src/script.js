8c59bc375cb2f22fa244dd5c6349a89c00f89ded
vlaru <dn210690nvs@gmail.com>
2025-05-27 13:35:45 +0300

    refactor: remove showDropList function and its import from script.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,10 @@
 import './sass/style.scss';
-import showDropList from './modules/ui/showDropList';
 import switchTheme from './modules/ui/switchTheme';
 import { createLogicGame } from './modules/core/logicGame';
 import initializeEventHandlers from './modules/dom/eventHandlers';
+
 document.addEventListener('DOMContentLoaded', () => {
   createLogicGame();
-  showDropList();
   switchTheme();
   initializeEventHandlers();
 });
