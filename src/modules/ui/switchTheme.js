1e1a7c071dca8529d323ba5763e4866e7326065f
vlaru <dn210690nvs@gmail.com>
2025-06-02 16:47:27 +0300

    feat: enhance theme switching functionality by saving user preference in localStorage and improving toggle logic

--- src/modules/ui/switchTheme.js
+++ src/modules/ui/switchTheme.js
@@ -1,5 +1,6 @@
 function switchTheme() {
   const darkModeToggle = document.getElementById('darkModeToggle');
+  const savedTheme = localStorage.getItem('dark-mode');
   const selectors = [
     '.container_game-tools',
     '.nav-elements-container',
@@ -9,11 +10,24 @@ function switchTheme() {
     'body',
   ];
 
-  darkModeToggle.addEventListener('change', () => {
+  function changeThemeMode() {
     document.querySelectorAll('.burger-element').forEach((el) => el.classList.toggle('dark-mode'));
     selectors.forEach((selector) => {
       document.querySelectorAll(selector).forEach((element) => element.classList.toggle('dark-mode'));
     });
+  }
+
+  if (savedTheme !== null) {
+    darkModeToggle.checked = savedTheme === 'true';
+    if (savedTheme === 'true') {
+      changeThemeMode();
+    }
+  }
+
+  darkModeToggle.addEventListener('change', () => {
+    const isDark = darkModeToggle.checked;
+    localStorage.setItem('dark-mode', isDark);
+    changeThemeMode();
   });
 }
 
